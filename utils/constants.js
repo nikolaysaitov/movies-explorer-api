ab1bd923938940dbaae14619fe41ac7fe4151c05
Nikolay Saitov <saitovkms@gmail.com>
2022-08-05 01:24:49 +0300

    rew222333

--- utils/constants.js
+++ utils/constants.js
@@ -1,26 +1,25 @@
 // const {
-//   dataMovies = 'mongodb://localhost:27017/kino',
-//   PORT = 3000,
 //   NODE_ENV,
 //   JWT_SECRET,
+//   DATA_BASE = 'mongodb://localhost:27017/kino',
 // } = process.env;
 
+// const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
+
 // module.exports = {
-//   dataMovies,
-//   PORT,
-//   NODE_ENV,
+//   addressMongoDB,
 //   JWT_SECRET,
 // };
-
 const {
   NODE_ENV,
   JWT_SECRET,
-  DATA_BASE = 'mongodb://localhost:27017/kino',
+  DATA_BASE,
 } = process.env;
 
+const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
 
 module.exports = {
   addressMongoDB,
-  JWT_SECRET,
+  secretKey,
 };
