c244e1515495f33debfec9c2a952f40c61023ae8
Nikolay Saitov <saitovkms@gmail.com>
2022-08-05 00:31:41 +0300

    rewe

--- utils/constants.js
+++ utils/constants.js
@@ -14,11 +14,13 @@
 
 const {
   NODE_ENV,
+  JWT_SECRET,
   DATA_BASE = 'mongodb://localhost:27017/kino',
 } = process.env;
 
-const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
+const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kiino';
 
 module.exports = {
   addressMongoDB,
+  JWT_SECRET,
 };
