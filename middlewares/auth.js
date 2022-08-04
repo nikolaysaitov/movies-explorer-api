c59a3b401501c5eb66696875e06fcb00483cc8e0
Nikolay Saitov <saitovkms@gmail.com>
2022-08-05 00:18:04 +0300

    rew234

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 const AuthorisationError = require('../errors/authorisation_error_401');
 
+const { JWT_SECRET } = require('../utils/constants');
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const { JWT_SECRET = 'some-secret-key' } = process.env;
+  // const { JWT_SECRET = 'some-secret-key' } = process.env;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new AuthorisationError('Необходима авторизация'));
