5e81a7fb79d2f611828bfdaa2b99a925b2859626
Nikolay Saitov <saitovkms@gmail.com>
2022-08-05 01:33:27 +0300

    redcvddds

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,9 @@
-// const jwt = require('jsonwebtoken');
-// const AuthorisationError = require('../errors/authorisation_error_401');
-
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   const { JWT_SECRET = 'some-secret-key' } = process.env;
-
-//   if (!authorization || !authorization.startsWith('Bearer')) {
-//     return next(new AuthorisationError('Необходима авторизация'));
-//   }
-//   const token = authorization.replace(/^\S+/, '').trim();
-
-//   let payload;
-
-//   try {
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     throw new AuthorisationError('Необходима авторизация');
-//   }
-
-//   req.user = payload; // записываем пейлоуд в объект запроса
-
-//   return next(); // пропускаем запрос дальше
-// };
 const jwt = require('jsonwebtoken');
 const AuthorisationError = require('../errors/authorisation_error_401');
-const { secretKey } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
+  const { JWT_SECRET = 'some-secret-key' } = process.env;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
     return next(new AuthorisationError('Необходима авторизация'));
@@ -37,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, secretKey);
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new AuthorisationError('Необходима авторизация');
   }
--- utils/constants.js
+++ utils/constants.js
@@ -1,25 +1,12 @@
-// const {
-//   NODE_ENV,
-//   JWT_SECRET,
-//   DATA_BASE = 'mongodb://localhost:27017/kino',
-// } = process.env;
-
-// const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
-
-// module.exports = {
-//   addressMongoDB,
-//   JWT_SECRET,
-// };
 const {
   NODE_ENV,
   JWT_SECRET,
-  DATA_BASE,
+  DATA_BASE = 'mongodb://localhost:27017/kino',
 } = process.env;
 
-const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
 
 module.exports = {
   addressMongoDB,
-  secretKey,
+  JWT_SECRET,
 };
